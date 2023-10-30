6561c83d35da0528999d07924f0839ed99cc3697
Eliyaz25 <eliyazaslover25@gmail.com>
2023-10-30 16:23:58 +0200

    Add project files.

---
+++ ViewModel/CityDB.cs
@@ -0,0 +1,44 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class CityDB : BaseDB
+    {
+        protected override BaseEntity NewEntity()
+        {
+            return new City();
+        }
+
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            City city = entity as City;
+            city.ID = int.Parse(reader["ID"].ToString());
+            city.Name = reader["cityName"].ToString();
+            return city;
+        }
+
+
+        public CityList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM tblCity";
+            CityList list = new CityList(ExecuteCommand());
+            return list;
+        }
+
+        public City SelectById(int id)
+        {
+            command.CommandText = "SELECT * FROM tblCity WHERE ID=" + id;
+            CityList list = new CityList(ExecuteCommand());
+            if (list.Count == 0)
+                return null;
+            return list[0];
+        }
+
+
+    }
+}
---
+++ ViewModel/UserDB.cs
@@ -0,0 +1,52 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class UserDB : BaseDB
+    {
+        protected override BaseEntity NewEntity()
+        {
+            return new User();
+        }
+
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            User user = entity as User;
+            user.ID = int.Parse(reader["ID"].ToString());
+            user.FirstName = reader["firstName"].ToString();
+            user.LastName = reader["lastName"].ToString();
+            user.Username = reader["username"].ToString();
+            user.Password = reader["password"].ToString();
+            user.Email = reader["email"].ToString();
+            user.Birthday = DateTime.Parse(reader["bDay"].ToString());
+            user.Gender = bool.Parse(reader["gender"].ToString());
+            user.IsManager = bool.Parse(reader["isManager"].ToString());
+            CityDB cityDB = new CityDB();
+            int cityID = int.Parse(reader["city"].ToString());
+            user.City = cityDB.SelectById(cityID);
+            return user;
+        }
+
+
+        public UserList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM tblUser";
+            UserList list = new UserList(ExecuteCommand());
+            return list;
+        }
+
+        public User SelectById(int id)
+        {
+            command.CommandText = "SELECT * FROM tblUser WHERE ID=" + id;
+            UserList list = new UserList(ExecuteCommand());
+            if (list.Count == 0)
+                return null;
+            return list[0];
+        }
+    }
+}
